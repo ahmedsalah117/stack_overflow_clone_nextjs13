f63dfe36ecb7feb65f0dc68d157f302598ef8a87
Ahmed Bahnasy <ahmedbahnasy1171998@gmail.com>
2023-10-19 18:13:15 +0300

    added routing authentication and prepared the layout

---
+++ app/(root)/ask-question/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AskQuestion() {
+  return <h1>Ask question AskQuestion</h1>;
+}
+
+export default Page;
--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <>
-      <h1 className="h1-bold">Hello Nextjs 13 we&apos;re coming soon</h1>
-      <h2 className="h2-bold">Hello Nextjs 13 we&apos;re coming soon</h2>
-      <h2 className="h2-semibold">Hello Nextjs 13 we&apos;re coming soon</h2>
-    </>
-  );
-}
