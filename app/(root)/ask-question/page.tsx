aaf233a51b22bf7ff65cedfdb301ab93d085f756
Ahmed Bahnasy <ahmedbahnasy1171998@gmail.com>
2024-04-04 02:50:43 +0200

    removed the fake userId from the ask question page

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,12 +1,11 @@
 import Question from "@/components/forms/Question";
 import { getUserById } from "@/lib/actions/user.action";
-// import { auth } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 import React from "react";
 
 async function Page() {
-  // const { userId } = auth();
-  const userId = "clerk123";
+  const { userId } = auth();
 
   if (!userId) redirect("/sign-in");
 
