71666c5e455792702a5ed0588fef2597c916a4de
Ahmed Bahnasy <ahmedbahnasy1171998@gmail.com>
2024-04-04 03:01:40 +0200

    added a console.log to the webhook file

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -55,6 +55,7 @@ export async function POST(req: Request) {
   // const { id } = evt.data;
   const eventType = evt.type;
 
+  console.log(eventType);
   if (eventType === "user.created") {
     const { id, email_addresses, first_name, last_name, image_url, username } =
       evt.data;
